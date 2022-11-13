ed521c6d033ed97b26caecbf56674f0691db7d18
Aleksandr Baranchuck <academws1@gmail.com>
2022-11-13 22:14:39 +0200

    feat: added btn gou course

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    Hello, world!
+  <div class="prose container mx-auto my-2">
+    <h1>
+      Hello, world!
+    </h1>
+    <nuxt-link to="/course" class="rounded text-white font-bold py-2 px-4 cursor-pointer no-underline bg-green-500">
+      GO TO COURSE
+    </nuxt-link>
   </div>
 </template>
